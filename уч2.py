300e392433dc95597aa150b30f69b8471b42e051
Арина <ponomarenko.itsmart@gmail.com>
2023-12-25 20:31:43 +0200

    Задание 2

--- уч2.py
+++ уч2.py
@@ -5,3 +5,12 @@ b = number % 10
 
 print (a)
 print(b)
+
+number1 = int(input("Ведіть трьохзначне число: "))
+a1 = number1 % 10
+b1 = number1 // 10 % 10
+c = number1 // 100
+
+print (a1)
+print(b1)
+print (c)
